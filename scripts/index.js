f93dd86edf588e82bf39fd52dd515fb92bebd85a
TPalma-2023 <palmaalv@msu.edu>
2023-06-20 21:37:57 -0400

    refactor modal functionality and styles

--- scripts/index.js
+++ scripts/index.js
@@ -28,12 +28,55 @@ const initialCards = [
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const modalButtonClose = document.querySelector(".modal__button-close");
-console.log(profileEditButton);
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
 
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+
   profileEditModal.classList.add("modal__opened");
 });
 
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.textContent = cardData.link;
+  console.log(cardImageEl);
+  cardImageEl.src = cardImageEl.textContent;
+  cardImageEl.alt = cardTitleEl.textContent;
+
+  return cardElement;
+}
+
 modalButtonClose.addEventListener("click", () => {
   profileEditModal.classList.remove("modal__opened");
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
 });
