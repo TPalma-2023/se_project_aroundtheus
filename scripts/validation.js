1207db4094701cc4b30ac9b8005bc60174eec4f3
TPalma-2023 <palmaalv@msu.edu>
2023-08-15 20:54:36 -0400

    Add form validation for all three modals

---
+++ scripts/validation.js
@@ -0,0 +1,112 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    return showInputError(formEl, inputEl, options);
+  }
+  hideInputError(formEl, inputEl, options);
+}
+
+function disableButton(inputEls, submitButton, inactiveButtonClass) {
+  if (hasInvalidInput(inputEls)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
+  }
+}
+
+function enableButton(inputEls, submitButton, inactiveButtonClass) {
+  if (!hasInvalidInput([...inputEls])) {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
+  }
+}
+
+function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+  if (hasInvalidInput(inputEls)) {
+    disableButton(inputEls, submitButton, inactiveButtonClass);
+    return;
+  }
+  enableButton(inputEls, submitButton, inactiveButtonClass);
+}
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(".popup__button");
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputEls, submitButton, options);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    setEventListeners(formEl, options);
+  });
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
+
+const openedModal = document.querySelectorAll(".modal__container");
+
+openedModal.onclick = function (event) {
+  console.log("yuh");
+  if (event.target.id != ".modal__container") {
+    console.log("Yuh");
+    closePopup();
+  }
+};
+
+document.addEventListener("click", (evt) => {
+  let clickClass = evt.target.classList;
+  let clickEl = evt.target;
+
+  clickClass.forEach((e) => {
+    if (e == "modal__opened") {
+      clickEl.classList.remove("modal__opened");
+    }
+  });
+});
+
+document.addEventListener("keydown", (evt) => {
+  if (evt.key == "Escape") {
+    closePopup(profileEditModal);
+    closePopup(addCardModal);
+    closePopup(modalImage);
+  }
+});
